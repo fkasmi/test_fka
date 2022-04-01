8b56b690ae7459547b4188616e1f8d4aa0534ac8
fkasmi <33028855+fkasmi@users.noreply.github.com>
2022-04-01 14:57:15 +0200

    rectangle

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-print("coucou")
+def aire_rectangle(largeur,longueur):
+    return largeur*longueur
+
 a=input()
-print("nice to meet you "+ a)
-print("branch")
+b=input()
+c=aire_rectangle(int(a),int(b))
+print("aire d'un rectangle de longueur {} et de largeur {} est de {}".format(a,b,c))
